b9ae2e0d8d6c1c70641560e283b695ccb9f8f4fb
Eric Risco de la Torre <erisco@icloud.com>
2017-04-30 17:09:03 +0200

    Creation, SimpleMultiplication, Equality, Hashable, Addition

---
+++ Money/Money/Typealias.swift
@@ -0,0 +1,11 @@
+//
+//  Typealias.swift
+//  Money
+//
+//  Created by Eric Risco de la Torre on 30/04/2017.
+//  Copyright © 2017 ERISCO. All rights reserved.
+//
+
+import Foundation
+
+typealias Currency = String
---
+++ Money/MoneyTests/MoneyTests.swift
@@ -0,0 +1,56 @@
+//
+//  MoneyTests.swift
+//  MoneyTests
+//
+//  Created by Eric Risco de la Torre on 30/04/2017.
+//  Copyright © 2017 ERISCO. All rights reserved.
+//
+
+import XCTest
+@testable import Money
+
+class MoneyTests: XCTestCase {
+    
+    var five: Money!
+    var otherFive: Money!
+    var ten: Money!
+    
+    override func setUp() {
+        super.setUp()
+        five = Money(amount: 5)
+        otherFive = Money(amount: 5)
+        ten = Money(amount: 10)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testCanCreateEuro(){
+        XCTAssertNotNil(Money())
+    }
+    
+    func testSimpleMultiplication(){
+        let ten = five.times(2)
+        XCTAssertEqual(ten._amount, 10)
+    }
+    
+    func testEquality(){
+        XCTAssertEqual(otherFive, otherFive)
+        XCTAssertEqual(five, otherFive)
+        
+        XCTAssertNotEqual(five, ten)
+    }
+    
+    func testThatObjectWithEqualHashAreEqual(){
+        XCTAssertEqual(five.hashValue, otherFive.hashValue)
+        XCTAssertNotEqual(ten.hashValue, otherFive.hashValue)
+    }
+    
+    func testSimpleAddition(){
+        XCTAssertEqual(ten, five.plus(otherFive))
+    }
+    
+    
+}
